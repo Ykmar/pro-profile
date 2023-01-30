58e63c1d88349d6c0df1c542708f3c1d6a6906ba
Marc COLLET <marc.collet2@hotmail.fr>
2023-01-30 21:28:03 +0100

    suppression fichier inutile + typage

--- app/Http/Controllers/LegalsController.php
+++ app/Http/Controllers/LegalsController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\View;
+
 class LegalsController extends Controller
 {
-    public function __invoke()
+    public function __invoke(): View
     {
         return view('legals.index');
     }
